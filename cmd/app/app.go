3e34fd63b12f799377fd5cf17afdacd4de748dd1
kjuiop <copying_y@naver.com>
2024-10-03 18:33:55 +0900

    [#1] feat: gin server add

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -5,11 +5,13 @@ import (
 	"log"
 	"momssi-apig-app/config"
 	"momssi-apig-app/internal/logger"
+	"momssi-apig-app/internal/server"
 	"sync"
 )
 
 type App struct {
 	cfg *config.EnvConfig
+	srv *server.Gin
 }
 
 func NewApplication(ctx context.Context) *App {
@@ -23,15 +25,20 @@ func NewApplication(ctx context.Context) *App {
 		log.Fatalf("fail to init slog err : %v", err)
 	}
 
+	srv := server.NewGinServer(cfg.Server)
+
 	app := &App{
 		cfg: cfg,
+		srv: srv,
 	}
 
 	return app
 }
 
 func (a *App) Start(wg *sync.WaitGroup) {
+	a.srv.Run(wg)
 }
 
 func (a *App) Stop(ctx context.Context) {
+	a.srv.Shutdown(ctx)
 }
